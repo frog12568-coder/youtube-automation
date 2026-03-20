d14b2889a9f94649ff02f70b8573fa10d00f26c9
frog12568-coder <frog12568@gmail.com>
2026-03-20 17:46:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 from gtts import gTTS
 import os
+import random
 
-# Step 1: Script
-script = "Amazing facts about space that will shock you!"
+# STEP 1: Story Parts
 
-# Step 2: Voice
-tts = gTTS(script)
+start = [
+    "Ek gareeb bacha tha.",
+    "Ek chota ladka tha.",
+    "Ek bacha tha jiske paas kuch nahi tha.",
+]
+
+middle = [
+    "Log uska mazak urate thay.",
+    "Sab kehte thay tum kuch nahi kar sakte.",
+    "Uske paas paise bhi nahi thay.",
+]
+
+ending = [
+    "Lekin usne himmat nahi hari aur aaj woh successful hai.",
+    "Magar usne mehnat ki aur sabko galat sabit kar diya.",
+    "Aur aaj woh lakho logon ke liye inspiration hai.",
+]
+
+# STEP 2: Combine story
+script = random.choice(start) + " " + random.choice(middle) + " " + random.choice(ending)
+
+print(script)
+
+# STEP 3: Voice
+tts = gTTS(script, lang='ur')
 tts.save("voice.mp3")
 
-# Step 3: Dummy video (1 image + audio)
-os.system("ffmpeg -loop 1 -i 17740101273351740548611328261147.jpg -i voice.mp3 -c:v libx264 -t 30 -pix_fmt yuv420p output.mp4")
+# STEP 4: Video
+os.system("ffmpeg -loop 1 -i image.jpg -i voice.mp3 -vf scale=720:1280 -c:v libx264 -t 30 -pix_fmt yuv420p output.mp4")
 
-print("Video created!")
+print("New random story video created!")
